f12252647fc501d0e35e3100c09937859775a6fb
RaidenAlfares13 <99805736+RaidenAlfares13@users.noreply.github.com>
2022-02-23 16:49:50 +0700

    Fixed Smth

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -108,7 +108,7 @@ class MainMenuState extends MusicBeatState
 
 		var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, "Osu Engine v" + psychEngineVersion, 12);
 		versionShit.scrollFactor.set();
-		versionShit.setFormat("FOnt OSD Mono", 16, FlxColor.WHITE, RIGHT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		versionShit.setFormat("FONT OSD Mono", 16, FlxColor.WHITE, RIGHT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
 		var versionShit:FlxText = new FlxText(12, FlxG.height - 24, 0, "Osu Mania' v" + Application.current.meta.get('version'), 12);
 		versionShit.scrollFactor.set();
